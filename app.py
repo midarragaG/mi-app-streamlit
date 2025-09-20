a9bf37ec4e399c71090e2d58a765d41d36ac8729
midarragaG <ma.idarraga13@gmail.com>
2025-09-20 14:10:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import matplotlib.pyplot as plt
 #Cargamos el modelo
 import pickle
 filename = 'modelo-reg.pkl'
-modelTree,modelKnn, modelNN,min_max_scaler variables = pickle.load(open(filename, 'rb'))
+modelTree,modelKnn, modelNN,min_max_scaler,variables = pickle.load(open(filename, 'rb'))
 
 #Cargamos los datos futuros
 #data = pd.read_csv("videojuegos-datosFuturos.csv")
